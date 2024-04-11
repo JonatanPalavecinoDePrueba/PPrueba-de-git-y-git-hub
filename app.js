2b0d442774669e2fc792abf8c7f5f0b63e326e67
Jonatan Palavecino <jnpr1992@hotmail.com>
2024-04-11 01:12:58 -0300

    jona agrego elementos al proyecto

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
 console.log("!hello word");
+
+const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+    console.log(element);
+}
